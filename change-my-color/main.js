26f9c94b25e34f0d7aedb9867d92c2ce3fb4437a
imac1 <irina.macovei@yahoo.fr>
2022-10-04 07:47:47 +0300

    redone according to instructions

--- change-my-color/main.js
+++ change-my-color/main.js
@@ -1,7 +1,5 @@
 let button = document.getElementById("change-color");
-let defaultColor = button.style.backgroundColor;
-let alternateDefault = button.style.backgroundColor
+
 button.addEventListener("click", (event) => {
-	defaultColor = button.style.backgroundColor =
-		defaultColor === alternateDefault ? "red" : alternateDefault;
+	button.classList.toggle('clicked')
 });
